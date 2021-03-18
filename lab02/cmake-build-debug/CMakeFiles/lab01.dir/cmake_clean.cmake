1b175b3d119e4b9d51b42d8de34ce446b427c954
“sy1wi4” <sylwia1301@onet.pl>
2021-03-18 21:23:55 +0100

    zad3 done

---
+++ lab02/cmake-build-debug/CMakeFiles/lab01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab01.dir/zad2/main.c.o"
+  "lab01"
+  "lab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
