2f4cea8aec213632decbc2f7565f75e9264e6155
“sy1wi4” <sylwia1301@onet.pl>
2021-03-24 23:12:42 +0100

    zad2 done

---
+++ lab03/zad2/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mylibrary.h"
+#include <sys/times.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+long double time_sec(clock_t time){
+    return (long double)(time) / sysconf(_SC_CLK_TCK);
+}
+
+void print_res(clock_t clock_start, clock_t clock_end, struct tms start_tms, struct tms end_tms){
+    printf("real %Lf\n", time_sec(clock_end - clock_start));
+    printf("user %Lf\n", time_sec(end_tms.tms_utime - start_tms.tms_utime));
+    printf("sys  %Lf\n\n", time_sec(end_tms.tms_stime - start_tms.tms_stime));
+}
+
+int main(int argc, char *argv[]) {
+    // ./main file1.txt:file2.txt file3.txt:file4.txt
+
+    struct tms start_tms;
+    struct tms end_tms;
+    clock_t clock_start;
+    clock_t clock_end;
+
+    clock_start = times(&start_tms);
+
+    pid_t pid;
+    printf("\n");
+    for (int i = 1; i < argc ; i++) {
+        char* file1 = (char*)calloc(strlen(argv[i]), sizeof(char));
+        char* file2 = (char*)calloc(strlen(argv[i]), sizeof(char));
+
+        pid = fork();
+        if (pid == 0) {
+            int file1_len = strchr(argv[i], ':') - argv[i];
+            for (int a = 0; a < file1_len; a++) {
+                file1[a] = argv[i][a];
+            }
+            for (int b = file1_len + 1; b < strlen(argv[i]); b++) {
+                file2[b - file1_len - 1] = argv[i][b];
+            }
+            merge_files(file1, file2);
+
+            exit(0);
+        }
+    }
+
+    for (int i = 1; i < argc ; i++) wait(NULL);
+
+    clock_end = times(&end_tms);
+    printf("\n\nTOTAL EXECUTION TIME\n");
+    print_res(clock_start, clock_end, start_tms, end_tms);
+
+    return 0;
+}
---
+++ lab03/zad2/mylibrary.c
@@ -0,0 +1,153 @@
+//
+// Created by sylwia on 3/10/21.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include "mylibrary.h"
+#include <stdio.h>
+#include <unistd.h>
+
+struct MainArray* create_main_arr(int size){
+    struct MainArray* arr = calloc(1, sizeof(struct MainArray));
+
+    arr->size = size;
+    arr->last_added_idx = -1;
+    arr->blocks = calloc(size, sizeof(struct Block));
+    printf("Created array of size %d\n\n", size);
+
+    return arr;
+}
+
+struct Block* create_merged_block(char* file_name1, char* file_name2){
+    struct Block* block = calloc(1, sizeof(struct Block));
+    FILE* file1 = fopen(file_name1, "r");
+    FILE* file2 = fopen(file_name2, "r");
+
+
+    if(file1 == NULL || file2 == NULL) {
+        perror("Cannot open file ");
+        exit(1);
+    }
+
+    char* line1 = (char*)calloc(256,sizeof(char));
+    char* line2 = (char*)calloc(256,sizeof(char));
+
+    FILE* temp = tmpfile();
+    temp = fopen("temp.txt","w");
+    int lines = 0;
+    while(fgets(line1, 256*sizeof(char), file1) && fgets(line2, 256*sizeof(char), file2)) {
+        fputs(line1,temp);
+        if (feof(file1)){
+            fputs("\n", temp);
+        }
+        fputs(line2,temp);
+        lines += 2;
+    }
+    fclose(temp);
+    fopen("temp.txt", "r");
+
+    block->rows = (char**)calloc(lines, sizeof(char*));
+
+    int idx = 0;
+    char* line = (char*)calloc(256,sizeof(char));
+    while(fgets(line, 256*sizeof(char), temp)){
+        char* row = (char*)calloc(256,sizeof(char));
+        strcpy(row,line);
+        block->rows[idx++] = row;
+
+    }
+    block->rows_number = idx;
+
+    fclose(file1);
+    fclose(file2);
+    fclose(temp);
+
+    return block;
+}
+
+
+void merge_files(char* file_name1, char* file_name2){
+
+    FILE* file1 = fopen(file_name1, "r");
+    FILE* file2 = fopen(file_name2, "r");
+
+//    printf("\nTO MERGE  %s %s\n", file_name1, file_name2);
+    if(file1 == NULL || file2 == NULL) {
+        perror("Cannot open file ");
+        exit(1);
+    }
+
+    char* line1 = (char*)calloc(256,sizeof(char));
+    char* line2 = (char*)calloc(256,sizeof(char));
+
+    FILE* temp = tmpfile();
+    int lines = 0;
+    while(fgets(line1, 256*sizeof(char), file1) && fgets(line2, 256*sizeof(char), file2)) {
+        fputs(line1,temp);
+        if (feof(file1)){
+            fputs("\n", temp);
+        }
+        fputs(line2,temp);
+        lines += 2;
+    }
+
+
+    rewind(temp);
+
+    char* buffer = calloc(256,sizeof(char));
+    while (fgets(buffer, 256, temp))
+        printf("PID: %d   %s", getpid(), buffer);
+
+    printf("\n");
+
+    fclose(file1);
+    fclose(file2);
+    fclose(temp);
+}
+
+int get_rows_number(struct Block* block){
+    return block->rows_number;
+}
+
+void remove_block(struct MainArray* arr, int idx){
+    if (arr->blocks[idx] == NULL) return;
+    free(arr->blocks[idx]);
+    arr->blocks[idx] = NULL;
+    printf("Removed block at idx %d\n\n", idx);
+}
+
+void remove_row(struct MainArray* arr, int block_idx, int row_idx){
+    if (arr->blocks[block_idx] == NULL) return;
+    if (arr->blocks[block_idx]->rows[row_idx] == NULL) return;
+    free(arr->blocks[block_idx]->rows[row_idx]);
+    arr->blocks[block_idx]->rows[row_idx] = NULL;
+    printf("Removed row at idx %d (block %d)\n\n", row_idx, block_idx);
+    print_block(arr->blocks[block_idx]);
+}
+
+void print_block(struct Block* block){
+    for (int j = 0; j < block->rows_number; j++){
+        if (block->rows[j] != NULL) {
+            printf("%d: %s\n", j, block->rows[j]);
+        }
+    }
+    printf("\n");
+}
+
+void print_main_arr(struct MainArray* arr){
+    struct Block *block;
+    for (int i = 0; i < arr->size; i++){
+        block = arr->blocks[i];
+        if (block == NULL) continue;
+        printf("BLOCK NUMBER %d\n", i);
+        for (int j = 0; j < block->rows_number; j++){
+            if (block->rows[j] != NULL) {
+                printf("%d: %s\n", j, block->rows[j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+
