6605e58488dd44548a39292bae2e8a5d30ea794f
“sy1wi4” <sylwia1301@onet.pl>
2021-04-10 02:12:05 +0200

    sudo kill me

--- lab04/zad3a/catcher.c
+++ lab04/zad3a/catcher.c
@@ -6,24 +6,26 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <stdlib.h>
 
 
 int caught = 0;
 char* mode;
 
 void handler_SIGUSR1(int signum){
-    printf("Received SIGUSR1\n");
     caught++;
+    printf("\nCatcher handler_SIGUSR1 here. New SIGUSR1 caught ([%d] so far)\n", caught);
 }
 
 void handler_SIGUSR2(int sig, siginfo_t *info, void *ucontext){
-    printf("Total received SIGUSR1 signals: %d", caught);
+    printf("--------------------------- ALL SIGNALS SENT -------------------------------------\n\n");
+    printf("Catcher received SIGUSR2, now is going to send back [%d] received SIGUSR1 signals.\n", caught);
 
     // get sender PID from siginfo_t
     pid_t sender_PID = info->si_pid;
     printf("Sender PID: %d\n", sender_PID);
 
-    // SIGSR2 handled - send back SIGUSR1 (caught times)
+    // SIGUSR2 handled - send back SIGUSR1 (caught times)
     for (int i = 0; i < caught; i++){
         if (strcmp(mode,"kill") == 0){
             kill(sender_PID, SIGUSR1);
@@ -58,6 +60,8 @@ void handler_SIGUSR2(int sig, siginfo_t *info, void *ucontext){
     else{
         printf("Wrong mode\n");
     }
+
+    exit(0);
 }
 
 
@@ -68,15 +72,46 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    printf("Catcher here! My PID: %d\n", getpid());
+    printf("\n\nCatcher here! My PID: %d\n", getpid());
 
     mode = argv[1];
     printf("Mode: %s\n", mode);
 
-    printf("\nCatcher waiting for SIGUSR1...\n");
+    printf("Catcher waiting for SIGUSR1...\n");
+
+    // sigaction() can block other signals until the current handler returns
+
+
+    // now we should set up handlers, etc.
+    struct sigaction act_SIGUSR1;
+    sigemptyset(&act_SIGUSR1.sa_mask);
+    act_SIGUSR1.sa_handler = handler_SIGUSR1;
+    sigaction(SIGUSR1, &act_SIGUSR1, NULL);
+
+    //SIGUSR2
+    struct sigaction act_SIGUSR2;
+    sigemptyset(&act_SIGUSR2.sa_mask);
+    act_SIGUSR2.sa_flags = SA_SIGINFO;
+    act_SIGUSR2.sa_sigaction  = handler_SIGUSR2;
+    sigaction(SIGUSR2, &act_SIGUSR2, NULL);
+
 
     // block other signals, wait by calling sigsuspend()
 
+    sigset_t mask;
+    sigfillset(&mask);   // initializes set including all signals
+    // remove SIGUSR1 & SIGUSR2 from mask
+    sigdelset(&mask, SIGUSR1);
+    sigdelset(&mask, SIGUSR2);
+//    if(strcmp(mode, "sigrt") == 0){
+//        sigdelset(&mask, SIGRTMIN+1);
+//        sigdelset(&mask, SIGRTMIN+2);
+//    }
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+
+//    sigsuspend(&mask);
+    while(1){}
 
     return 0;
 }
--- lab04/zad3a/sender.c
+++ lab04/zad3a/sender.c
@@ -6,6 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <unistd.h>
+
+int caught = 0;
+int to_send;
+
+
+void handler_SIGUSR1(int signum) {
+    printf("Sender handler_SIGUSR1 here. New SIGUSR1 caught.\n");
+    caught++;
+}
+
+
+void handler_SIGUSR2(int signum) {
+    printf("\n--------------------------- ALL SIGNALS SENT BACK --------------------------------\n\n");
+    printf("Sender handler_SIGUSR2 here. SIGUSR2 caught.\n");
+    printf("Sender caught [%d] SIGUSR signals, should be [%d].\n", caught, to_send);
+    printf("\n///////// EXIT /////////\n\n");
+    exit(0);
+}
+
 
 int main(int argc, char* argv[]){
 
@@ -16,16 +36,34 @@ int main(int argc, char* argv[]){
 
 
     int catcher_PID = atoi(argv[1]);
-    int to_send = atoi(argv[2]);
+    to_send = atoi(argv[2]);
     char* mode = argv[3];
+
+    printf("\n\nSender here! My PID: %d\n", getpid());
     printf("Catcher PID: %d\n", catcher_PID);
     printf("Number of signals to send: %d\n", to_send);
     printf("Mode: %s\n", mode);
 
+
+    // SIGUSR1
+    struct sigaction act_SIGUSR1;
+    sigemptyset(&act_SIGUSR1.sa_mask);
+    act_SIGUSR1.sa_handler = handler_SIGUSR1;
+    sigaction(SIGUSR1, &act_SIGUSR1, NULL);
+
+    //SIGUSR2
+    struct sigaction act_SIGUSR2;
+    sigemptyset(&act_SIGUSR2.sa_mask);
+    act_SIGUSR2.sa_handler = handler_SIGUSR2;
+    sigaction(SIGUSR2, &act_SIGUSR2, NULL);
+
+
+    printf("\nSender going to send %d SIGUSR1 signals ......\n", to_send);
     for (int i = 0; i < to_send; i++) {
 
         if (strcmp(mode, "kill") == 0) {
             kill(catcher_PID, SIGUSR1);
+
         }
 
         else if (strcmp(mode, "queue") == 0) {
@@ -44,9 +82,14 @@ int main(int argc, char* argv[]){
     }
 
 
+    printf("Sender sent all [%d] SIGUSR1 signals.\n\n", to_send);
+
     // send SIGUSR2
 
+    printf("Now sender is going to send SIGUSR2\n");
+
     if (strcmp(mode, "kill") == 0) {
+        printf("GOING TO KILL YOU CATCHER (pid: %d)\n\n", catcher_PID);
         kill(catcher_PID, SIGUSR2);
     }
 
